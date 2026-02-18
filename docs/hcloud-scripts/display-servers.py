3793cab42384be12d52b6b47bbe262f10e0d2c0f
Ronas Olsen <rono@itu.dk>
2026-02-18 20:18:55 +0100

    More scripts. Server starts with SSH keys set.

--- docs/hcloud-scripts/display-servers.py
+++ docs/hcloud-scripts/display-servers.py
@@ -1,13 +1,14 @@
 import argparse
+import sys
 
 from hcloud import Client
 from hcloud.images import Image
 from hcloud.server_types import ServerType
 
-# This script displays the reference names of Hetzner data centres.
+# This script displays the status of all project servers.
 def main():
     parser = argparse.ArgumentParser(
-        description="# This script displays the reference names of Hetzner data centres."
+        description="This script displays the status of all project servers."
     )
 
     parser.add_argument(
@@ -19,18 +20,17 @@ def main():
 
     args = parser.parse_args()
 
-    # Closely follows the example found at:
-    # https://github.com/hetznercloud/hcloud-python
     client = Client(
         token=args.apikey,
-        application_name="get-locations.py",
+        application_name="display-servers.py",
         application_version="v1.0.0"
     )
 
-    locationResponse = client.locations.get_all()
-    print("*** Locations:")
-    for l in locationResponse:
-        print(l.name)
+    # List your servers
+    servers = client.servers.get_all()
+    print("*** Active servers:")
+    for server in servers:
+        print(f"{server.id=} {server.name=} {server.status=}")
 
 if __name__ == "__main__":
     main()
--- docs/hcloud-scripts/make-devops-serv.py
+++ docs/hcloud-scripts/make-devops-serv.py
@@ -5,9 +5,11 @@ from hcloud import Client
 from hcloud.images import Image
 from hcloud.server_types import ServerType
 
+# This script creates a server matching the one we currently use for 
+# ITU-Minitwit. (February 18th 2026)
 def main():
     parser = argparse.ArgumentParser(
-        description="Python script that creates our ITU-Minitwit server on Hetzner."
+        description="Python script that creates a server on Hetzner for ITU-Minitwit."
     )
 
     parser.add_argument(
@@ -27,31 +29,34 @@ def main():
 
     # Closely follows the example found at:
     # https://github.com/hetznercloud/hcloud-python
+    # See Python API docs at:
+    # https://hcloud-python.readthedocs.io/en/stable/index.html
+
     client = Client(
         token=args.apikey,
         application_name="make-devops-serv.py",
         application_version="v1.0.0"
     )
 
-    volumeResponse = client.volumes.create(
-        size=10, # GB
-        location=client.locations.get_by_name("hel1"),
-        name = args.servername + "-volume"
+    volumeResponse = client.volumes.create( # Provision a 10 GB volume for the server database. 
+        size=10,
+        name = args.servername + "-volume",
+        location=client.locations.get_by_name("hel1")  # Provision in the Helsinki data center.
     )
 
-    # Creates a server with Docker CE image
-    serverResponse = client.servers.create(
+    serverResponse = client.servers.create( # Creates a server with Docker CE image
         name=args.servername,
-        server_type=ServerType(name="cax11"),
-        image=Image(name="docker-ce"),
-        location=client.locations.get_by_name("hel1"),
-        volumes=[volumeResponse.volume]
+        server_type=ServerType(name="cax11"),          # Provision the small CAX11 ARM64 server.
+        image=Image(name="docker-ce"),                 # Use the default Ubuntu image with Docker installed. 
+        location=client.locations.get_by_name("hel1"), # Provision in the Helsinki data center. 
+        volumes=[volumeResponse.volume],               # This will mount the volume we just created.
+        ssh_keys=client.ssh_keys.get_all()             # ALL public SSH keys in the project will be authorized for root logon!
     )
 
     server = serverResponse.server
-    print("*** New server created:")
+    print("*** New server being created:")
     print(f"{server.id=} {server.name=} {server.status=}")
-    print(f"root password: {serverResponse.root_password}")
+    print(f"root password: {serverResponse.root_password}") # Only relevant if we did not set any SSH keys. 
 
     # List your servers
     servers = client.servers.get_all()
