2fc617549ccea4e498772b4b13f239a2e2fc0253
Ronas Olsen <rono@itu.dk>
2026-02-26 17:51:17 +0100

    Tests for register POST endpoint.
    5 tests fail. Covers 40% of SimulatorController.

--- test/Chirp.TestIntegration/SimulatorApiTests.cs
+++ test/Chirp.TestIntegration/SimulatorApiTests.cs
@@ -1,3 +1,6 @@
+using Chirp.Core.DTOs;
+using Chirp.Core.Interfaces;
+
 namespace Chirp.TestIntegration;
 
 public class SimulatorApiTests : AbstractIntegration
@@ -13,8 +16,8 @@ public class SimulatorApiTests : AbstractIntegration
         }
 
         var response = await _client.GetAsync("/api/latest");
-        response.StatusCode.Should().Be(HttpStatusCode.OK);
 
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
         var content = await response.Content.ReadFromJsonAsync<Core.DTOs.LatestResponse>();
         content.Should().NotBeNull();
         content.Latest.Should().Be(-1);
@@ -31,11 +34,11 @@ public class SimulatorApiTests : AbstractIntegration
         }
 
         int testLatest = new Random().Next();
-
         await _client.GetAsync($"/api/msgs?latest={testLatest}");
-        var response = await _client.GetAsync("/api/latest");
-        response.StatusCode.Should().Be(HttpStatusCode.OK);
 
+        var response = await _client.GetAsync("/api/latest");
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
         var content = await response.Content.ReadFromJsonAsync<Core.DTOs.LatestResponse>();
         content.Should().NotBeNull();
         content.Latest.Should().Be(testLatest);
@@ -57,8 +60,123 @@ public class SimulatorApiTests : AbstractIntegration
         }
 
         var response = await _client.GetAsync(requestUri);
+
         response.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
-        var content = await response.Content.ReadFromJsonAsync<Core.DTOs.ErrorResponse>();
+        var content = await response.Content.ReadFromJsonAsync<ErrorResponse>();
+        content.Should().NotBeNull();
+        content.Status.Should().Be(500);
+        content.ErrorMsg.Should().Be("Internal server error");
+    }
+
+    [Fact]
+    public async Task Register_CorrectRequest()
+    {
+        using (var scope = _factory.Services.CreateScope())
+        {
+            var dbContext = scope.ServiceProvider.GetRequiredService<CheepDBContext>();
+            await dbContext.Database.EnsureDeletedAsync(); // Clear previous data
+            await dbContext.Database.EnsureCreatedAsync(); // Recreate the database
+        }
+
+        var response = await _client.PostAsJsonAsync(
+            "/api/register?latest=1", 
+            new RegisterRequest
+            {
+                Username = "Rono ITU",
+                Email = "rono@example.com",
+                Pwd = "2a3b&4Cd",
+            }
+        );
+
+        response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+
+        using (var scope = _factory.Services.CreateScope())
+        {
+            var authorService = scope.ServiceProvider.GetRequiredService<IAuthorService>();
+            var newAuthor = await authorService.FindAuthorByName("Rono ITU");
+            newAuthor.Should().NotBeNull();
+            newAuthor.Name.Should().Be("Rono ITU");
+            newAuthor.Email.Should().Be("rono@example.com");
+        }
+    }
+
+    [Fact]
+    public async Task Register_UserAlreadyExists()
+    {
+        // Easy way to prepare for test.
+        await Register_CorrectRequest();
+
+        var response = await _client.PostAsJsonAsync(
+            "/api/register?latest=2", 
+            new RegisterRequest
+            {
+                Username = "Rono ITU",
+                Email = "rono2@example.com",
+                Pwd = "2a3b&4Cd",
+            }
+        );
+
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+        // Existing user should still be found.
+        using (var scope = _factory.Services.CreateScope())
+        {
+            var authorService = scope.ServiceProvider.GetRequiredService<IAuthorService>();
+            var newAuthor = await authorService.FindAuthorByName("Rono ITU");
+            newAuthor.Should().NotBeNull();
+            newAuthor.Name.Should().Be("Rono ITU");
+            newAuthor.Email.Should().Be("rono@example.com");
+        }
+    }
+
+    [Theory]
+    [InlineData("", "rono@example.com", "2a3b&4Cd")]
+    [InlineData("Rono ITU", "", "2a3b&4Cd")]
+    [InlineData("Rono ITU", "rono@example.com", "")]
+    public async Task Register_BadRequest_EmptyFields(string userName, string email, string pwd)
+    {
+        using (var scope = _factory.Services.CreateScope())
+        {
+            var dbContext = scope.ServiceProvider.GetRequiredService<CheepDBContext>();
+            await dbContext.Database.EnsureDeletedAsync(); // Clear previous data
+            await dbContext.Database.EnsureCreatedAsync(); // Recreate the database
+        }
+
+        var response = await _client.PostAsJsonAsync(
+            "/api/register?latest=1", 
+            new RegisterRequest
+            {
+                Username = userName,
+                Email = email,
+                Pwd = pwd,
+            }
+        );
+
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task Register_CatchServerSideExceptions()
+    {
+        using (var scope = _factory.Services.CreateScope())
+        {
+            var dbContext = scope.ServiceProvider.GetRequiredService<CheepDBContext>();
+            await dbContext.Database.EnsureDeletedAsync();
+            // Left in this state will raise an exception for most calls.
+        }
+
+        var response = await _client.PostAsJsonAsync(
+            "/api/register?latest=1", 
+            new RegisterRequest
+            {
+                Username = "Rono ITU",
+                Email = "rono@example.com",
+                Pwd = "2a3b&4Cd",
+            }
+        );
+
+        response.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
+        var content = await response.Content.ReadFromJsonAsync<ErrorResponse>();
         content.Should().NotBeNull();
         content.Status.Should().Be(500);
         content.ErrorMsg.Should().Be("Internal server error");
