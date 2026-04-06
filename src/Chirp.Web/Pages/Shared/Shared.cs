194471abdec5ed350b5417bc8f8ab4702846f861
Ronas Olsen <rono@itu.dk>
2026-04-06 20:49:55 +0200

    Using namespace Chirp.Web.Pages

--- src/Chirp.Web/Pages/Shared/Shared.cs
+++ src/Chirp.Web/Pages/Shared/Shared.cs
@@ -1,6 +1,8 @@
 using System.Text.RegularExpressions;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
+namespace Chirp.Web.Pages;
+
 public static class Shared
 {
     private static readonly Regex _ConvertLinksToAnchors_regex = new Regex(@"((http|https):\/\/)?(www\.)?[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(\S*[^.,\s])?", RegexOptions.Compiled | RegexOptions.IgnoreCase, matchTimeout: TimeSpan.FromMilliseconds(500));
